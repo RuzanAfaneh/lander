64ee50a74ceec01366733ca11c12e1def9f6372b
RuzanAfaneh <ruzanafaneh@gmail.com>
2022-01-07 20:16:21 +0200

    add icons block

--- components/HeroVideo.vue
+++ components/HeroVideo.vue
@@ -1,6 +1,9 @@
 <template>
   <video class="hero_video" autoplay muted loop nocontrols>
-    <source src="https://video-previews.elements.envatousercontent.com/files/5ce7466f-36f6-48bc-b33d-01e5cba6c72f/video_preview_h264.mp4" type="video/mp4" />
+    <source
+      src="https://video-previews.elements.envatousercontent.com/files/5ce7466f-36f6-48bc-b33d-01e5cba6c72f/video_preview_h264.mp4"
+      type="video/mp4"
+    />
   </video>
 </template>
 
@@ -12,9 +15,8 @@ export default {
 
 <style lang="scss" scoped>
 .hero_video {
+  height: auto;
   max-width: 100vw;
-  min-height: 100vh;
   width: 100%;
-  height: 100%;
 }
 </style>
