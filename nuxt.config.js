bebaef47e1b975a3c28907c5aed4cccaf6a2a0c1
RuzanAfaneh <ruzanafaneh@gmail.com>
2022-01-08 17:05:22 +0200

    add icons block

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,6 +46,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extractCSS: true,
     loaders: {
       sass: {
         implementation: require('sass'),
